4b91a3a642c92cbd852d0d97773bfc21cf11aeb8
Johan Jassed Jimenez Garcia <81938895+jassed18@users.noreply.github.com>
2024-08-27 17:52:51 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Auth from './Auth/Auth';
+import Chat from './components/Chat/ChatApp';
+import { AuthProvider } from './context/AuthContext';
+import PrivateRoute from './components/PrivateRoute';
+import Navbar from './components/Navbar/Navbar';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+          <Routes>
+            {/* Ruta para autenticación */}
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/login" element={<Auth />} />
+
+            {/* Ruta para el chat protegida */}
+            <Route path="/chat" element={
+              <>
+                <Navbar />  {/* Muestra el Navbar solo en la ruta /chat */}
+                <PrivateRoute element={<Chat />} />
+              </>
+            } />
+
+            {/* Redirige desde la raíz a la ruta de autenticación */}
+            <Route path="/" element={<Auth />} />
+          </Routes>
+      </Router>
+    </AuthProvider>
   );
-}
+};
 
 export default App;
